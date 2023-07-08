3b924b9c44dd0b2bb1b1f6a9f9aae1c321415f39
Jiaming <Jiamingw29@gmail.com>
2023-07-07 20:06:30 -0400

    updated url link part 2

--- app/components/Audio.js
+++ app/components/Audio.js
@@ -1,5 +1,7 @@
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+
 async function fetchAudio(id, group) {
-    const res = await fetch(`/api/audio/search?id=${id}&group=${group}`)
+    const res = await fetch(process.env.URL+`/api/audio/search?id=${id}&group=${group}`)
     return res.json()
 }
 import React from 'react';
