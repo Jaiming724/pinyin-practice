9fed9c96244de3b6ce591127f5d1f9f9029e2d73
Jiaming <Jiamingw29@gmail.com>
2023-07-08 18:40:13 -0400

    remove local api calls

--- app/components/Audio.js
+++ app/components/Audio.js
@@ -1,13 +1,15 @@
 import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import {getAudioLink} from "../util/AudioService"
 
-async function fetchAudio(id, group) {
-    const res = await fetch(process.env.URL+`/api/audio/search?id=${id}&group=${group}`)
-    return res.json()
+function fetchAudio(id, group) {
+
+    return getAudioLink(id, group)
 }
+
 import React from 'react';
 
-async function Audio({id, group}) {
-    const audios = await fetchAudio(id,group);
+function Audio({id, group}) {
+    const audios = fetchAudio(id, group);
     return (
         <div>{audios}
         </div>
---
+++ app/util/AudioService.js
@@ -0,0 +1,5 @@
+import first_audio from "./first_audio.json"
+
+export function getAudioLink(id, group) {
+    return first_audio[id]
+}
