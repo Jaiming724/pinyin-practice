1672c1c213e4bf936279ed9ead3cd85a6dad397e
Jiaming <Jiamingw29@gmail.com>
2024-05-11 19:09:10 -0400

    added navbar

--- app/groups/page.js
+++ app/groups/page.js
@@ -1,15 +1,18 @@
-
 import React from 'react';
 import QuizCard from "@/app/components/QuizCard";
 import {useSearchParams} from "next/navigation";
-import { usePathname } from 'next/navigation'
+import {usePathname} from 'next/navigation'
+import NavBar from "@/app/components/NavBar";
 
 function Group1Page({searchParams}) {
     console.log("group")
     console.log(searchParams?.["something"])
     return (
-        <div className="flex items-center justify-center h-screen">
-            <QuizCard group={searchParams?.["something"]}></QuizCard>
+        <div>
+            <NavBar></NavBar>
+            <div className="flex items-center justify-center h-screen">
+                <QuizCard group={searchParams?.["something"]}></QuizCard>
+            </div>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import {HeartIcon, BugAntIcon, CloudIcon, FaceSmileIcon} from "@heroicons/react/
 import Link from "next/link";
 import React from "react";
 import Image from 'next/image'
+import NavBar from "@/app/components/NavBar";
 
 const actions = [{
     title: 'A Group',
@@ -66,31 +67,35 @@ function classNames(...classes) {
 }
 
 export default function Example() {
-    return (<div className="flex items-center justify-center h-screen">
-        <div
-            className=" divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
-            {actions.map((action, actionIdx) => (<div
-                key={action.title}
-                className={classNames(actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', actionIdx === 1 ? 'sm:rounded-tr-lg' : '', actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '', actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500')}
-            >
-                <Link href={`./groups?something=${action.group}`}>
+    return (
+        <div>
+            <NavBar></NavBar>
+            <div className="flex items-center justify-center h-screen">
+                <div
+                    className=" divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+                    {actions.map((action, actionIdx) => (<div
+                        key={action.title}
+                        className={classNames(actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', actionIdx === 1 ? 'sm:rounded-tr-lg' : '', actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '', actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500')}
+                    >
+                        <Link href={`./groups?something=${action.group}`}>
 
-                    <div>
+                            <div>
             <span
                 className={classNames(action.iconBackground, action.iconForeground, 'inline-flex rounded-lg p-3 ring-4 ring-white')}>{action.title}
                 {/*<action.icon className="h-6 w-6" aria-hidden="true"/>*/}
             </span>
-                    </div>
-                    <div className="mt-2">
-                        <h3 className="text-base font-semibold leading-6 text-gray-900">
-                        </h3>
-                        <p className="mt-2 text-sm text-gray-500">
-                            {action.description}
-                        </p>
-                    </div>
-                </Link>
-            </div>))}
-        </div>
+                            </div>
+                            <div className="mt-2">
+                                <h3 className="text-base font-semibold leading-6 text-gray-900">
+                                </h3>
+                                <p className="mt-2 text-sm text-gray-500">
+                                    {action.description}
+                                </p>
+                            </div>
+                        </Link>
+                    </div>))}
+                </div>
 
-    </div>)
+            </div>
+        </div>)
 }
